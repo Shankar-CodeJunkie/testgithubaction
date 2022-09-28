5d4a9da25ff9aaa23219cccdb9680685805b2eef
SHANKAR VENKATACHALAM <svenkat7@in.ibm.com>
2022-09-28 20:33:14 +0530

    add octokit-auth

--- actions/post-comments/index.js
+++ actions/post-comments/index.js
@@ -1,21 +1,25 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { createActionAuth } = require("@octokit/auth-action");
 
 
 
 (
-    async() => {
+    async () => {
+
         try {
-            core.notice('Calling the action named post comments', github.token)
-            const token = core.getInput(github.token, {
+            const auth = createActionAuth();
+            const authentication = await auth();
+            core.notice('Calling the action named post comments',authentication.tokenType )
+            const token = core.getInput(authentication.token, {
                 required: true,
-              });
+            });
             const octokit = new github.GitHub(token);
             const {
                 data: { login },
-              } = await octokit.rest.users.getAuthenticated();
-            core.notice('logged in user is ', login);  
-        }catch(e) {
+            } = await octokit.rest.users.getAuthenticated();
+            core.notice('logged in user is ', login);
+        } catch (e) {
             core.setFailed('heyerr:', JSON.stringify(e));
         }
     }
