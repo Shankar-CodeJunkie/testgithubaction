adb1b87f28fecc204a693047a0edb99ea4b10dad
SHANKAR VENKATACHALAM <svenkat7@in.ibm.com>
2022-09-28 19:32:44 +0530

    changed actions

--- actions/post-comments/index.js
+++ actions/post-comments/index.js
@@ -6,6 +6,11 @@ const github = require('@actions/github');
     async() => {
         try {
             core.notice('Calling the action named post comments')
+            let octokit = github.getOctokit(secrets.GITHUB_TOKEN)
+            const {
+                data: { login },
+              } = await octokit.rest.users.getAuthenticated();
+            core.notice('logged in user is ', login);  
         }catch(e) {
             core.setFailed(e.message);
         }
