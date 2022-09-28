1df57d6185203c0d47df19bdf452d2518208814a
SHANKAR VENKATACHALAM <svenkat7@in.ibm.com>
2022-09-28 20:19:40 +0530

    fix

--- actions/post-comments/index.js
+++ actions/post-comments/index.js
@@ -6,7 +6,7 @@ const github = require('@actions/github');
 (
     async() => {
         try {
-            core.notice('Calling the action named post comments')
+            core.notice('Calling the action named post comments', github.token)
             const token = core.getInput(github.token, {
                 required: true,
               });
