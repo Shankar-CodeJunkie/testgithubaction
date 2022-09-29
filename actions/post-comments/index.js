38bfb784c78790853a1c4944fd9a9b0d5f63513e
SHANKAR VENKATACHALAM <svenkat7@in.ibm.com>
2022-09-29 16:39:33 +0530

    fix2

--- actions/post-comments/index.js
+++ actions/post-comments/index.js
@@ -11,31 +11,13 @@ const { createActionAuth } = require("@octokit/auth-action");
             core.notice(core.getInput('GITHUB_TOKEN', {required: true}));
             let githubtoken = core.getInput('GITHUB_TOKEN', {required: true});
             const octokit = github.getOctokit(githubtoken);
-            const { data: pullRequest } = await octokit.rest.pulls.get({
-                owner: 'octokit',
-                repo: 'rest.js',
-                pull_number: 123,
-                mediaType: {
-                  format: 'diff'
-                }
-            });
-            console.log(pullRequest);
-            //const { ACCESS_TOKEN } = process.env
-            //core.notice(process.env.GITHUB_TOKEN)
-            
-            //const { payload, sha } = github.context
-            //const { repository } = payload
-            //const octokit = new github.GitHub({auth: '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'})
-            
-            //console.log(octokit)  
-
-            /*const commit = await octokit.git.getCommit({
-                owner: repository.owner.login,
-                repo: repository.name,
-                commit_sha: sha
+            const response = await octokit.rest.pulls.createReviewComment({
+                owner: 'Shankar-CodeJunkie',
+                repo: 'testgithubaction',
+                pull_number: 1
             })
-            console.log(commit);
-            core.notice(commit)*/
+            console.log(response);
+            
 
         } catch (e) {
             core.setFailed('heyerr:');
