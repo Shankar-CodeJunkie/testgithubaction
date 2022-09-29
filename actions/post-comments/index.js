cebdaa5c0d7f6d0d6b4d83611f523ec960d02775
SHANKAR VENKATACHALAM <svenkat7@in.ibm.com>
2022-09-29 16:45:04 +0530

    fix4

--- actions/post-comments/index.js
+++ actions/post-comments/index.js
@@ -11,13 +11,18 @@ const { createActionAuth } = require("@octokit/auth-action");
             core.notice(core.getInput('GITHUB_TOKEN', {required: true}));
             let githubtoken = core.getInput('GITHUB_TOKEN', {required: true});
             const octokit = github.getOctokit(githubtoken);
-            const response = await octokit.rest.pulls.createReviewComment({
+            await octokit.request('POST /repos/Shankar-CodeJunkie/testgithubaction/pulls/{pull_number}/comments', {
                 owner: 'Shankar-CodeJunkie',
                 repo: 'testgithubaction',
-                pull_number: 1
-            })
-            console.log(response);
-            
+                pull_number: '1',
+                body: 'Great stuff!',
+                commit_id: '26446e9504f6e7b79eeb439f81b810ee47eab535',
+                path: 'file1.txt',
+                start_line: 1,
+                start_side: 'RIGHT',
+                line: 2,
+                side: 'RIGHT'
+              })
 
         } catch (e) {
             core.setFailed('heyerr:');
