45378582b019c992e45f93aac7c4569f4cea483c
SHANKAR VENKATACHALAM <svenkat7@in.ibm.com>
2022-09-29 13:13:15 +0530

    fix

--- actions/post-comments/index.js
+++ actions/post-comments/index.js
@@ -7,14 +7,7 @@ const { createActionAuth } = require("@octokit/auth-action");
     async () => {
 
         try {
-            const { context } = github;
-            const token = core.getInput('GITHUB_TOKEN', {
-              required: true,
-            });
-            core.notice('Calling the action named post comments', github.token )
-            core.notice(token)
-            const { issue } = context.payload;
-            core.notice('logged in user is ', login);
+           core.notice(github);
         } catch (e) {
             core.setFailed('heyerr:');
             core.setFailed(JSON.stringify(e))
