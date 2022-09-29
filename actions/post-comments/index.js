42c9b19fa4a7fe955342f8fb06e3d6498a51ab5d
SHANKAR VENKATACHALAM <svenkat7@in.ibm.com>
2022-09-29 15:15:47 +0530

    fix

--- actions/post-comments/index.js
+++ actions/post-comments/index.js
@@ -9,7 +9,17 @@ const { createActionAuth } = require("@octokit/auth-action");
             const { ACCESS_TOKEN } = process.env
             console.log(process.env.GITHUB_TOKEN)
             //core.notice('shankar', env);
-            
+            const octokit = new github.GitHub(ACCESS_TOKEN)
+            const { payload, sha } = github.context
+            const { repository } = payload
+
+            const commit = await octokit.git.getCommit({
+                owner: repository.owner.login,
+                repo: repository.name,
+                commit_sha: sha
+            })
+            console.log(commit);
+            core.notice(commit)
 
         } catch (e) {
             core.setFailed('heyerr:');
