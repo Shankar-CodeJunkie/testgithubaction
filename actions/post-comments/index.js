1617fb77d37af99c915c173fbd3885fd2919b002
SHANKAR VENKATACHALAM <svenkat7@in.ibm.com>
2022-09-29 15:17:37 +0530

    fix

--- actions/post-comments/index.js
+++ actions/post-comments/index.js
@@ -12,14 +12,17 @@ const { createActionAuth } = require("@octokit/auth-action");
             const octokit = new github.GitHub(ACCESS_TOKEN)
             const { payload, sha } = github.context
             const { repository } = payload
+            console.log(payload)
+            console.log(sha)
+            console.log(repository)
 
-            const commit = await octokit.git.getCommit({
+            /*const commit = await octokit.git.getCommit({
                 owner: repository.owner.login,
                 repo: repository.name,
                 commit_sha: sha
             })
             console.log(commit);
-            core.notice(commit)
+            core.notice(commit)*/
 
         } catch (e) {
             core.setFailed('heyerr:');
