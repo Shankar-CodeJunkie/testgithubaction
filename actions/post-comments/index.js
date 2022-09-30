0c9ca19ecb75f3c32ca983586b1570e96e6bc683
SHANKAR VENKATACHALAM <svenkat7@in.ibm.com>
2022-09-30 12:56:29 +0530

    fix12

--- actions/post-comments/index.js
+++ actions/post-comments/index.js
@@ -1,7 +1,8 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const { createActionAuth } = require("@octokit/auth-action");
-
+let githubtoken = core.getInput('GITHUB_TOKEN', {required: true});
+const octokit = github.getOctokit(githubtoken);
 
 (
     async () => {
@@ -18,7 +19,6 @@ const { createActionAuth } = require("@octokit/auth-action");
             })
             //console.log(response.data.title)
             sendComments(
-                octokit,
                 'Shankar-CodeJunkie',
                 'testgithubaction',
                 1,
@@ -26,12 +26,18 @@ const { createActionAuth } = require("@octokit/auth-action");
             )
 
             let pullRequests = await getPullRequests(
-                octokit,
                 'Shankar-CodeJunkie',
                 'testgithubaction',
             )
 
-            core.notice(pullRequests)
+            let releaseDetails = await getReleases(
+                'Shankar-CodeJunkie',
+                'testgithubaction'
+            )
+
+            core.notice(pullRequests);
+            core.notice('------------');
+            core.notice(releaseDetails)
         
 
         } catch (e) {
@@ -41,7 +47,7 @@ const { createActionAuth } = require("@octokit/auth-action");
     }
 )();
 
-function sendComments(octokit, orgName, repoName, issue_number, message) {
+function sendComments(orgName, repoName, issue_number, message) {
     octokit.rest.issues.createComment({
         owner: orgName,
         repo: repoName,
@@ -53,7 +59,7 @@ function sendComments(octokit, orgName, repoName, issue_number, message) {
     
 }
 
-async function getPullRequests(octokit, orgName, repoName) {
+async function getPullRequests(orgName, repoName) {
     let pullRequests = await octokit.rest.pulls.list({
         owner: orgName,
         repo: repoName,
@@ -61,4 +67,15 @@ async function getPullRequests(octokit, orgName, repoName) {
 
     return pullRequests.data
 
+}
+
+async function getReleases(owner, repo) {
+    let releases = [];
+    let releaseInfo = await octokit.request(
+        'GET /repos/{owner}/{repo}/releases',
+        {owner: 'OWNER',
+        repo: 'REPO'}
+    )
+    let releasesArray = releaseInfo.data.map(x => x.tag_name);
+    return releasesArray
 }
