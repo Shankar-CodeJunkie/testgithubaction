75d1b43bdc57da33ff86087a4caca9cdffa35353
SHANKAR VENKATACHALAM <svenkat7@in.ibm.com>
2022-09-30 12:59:38 +0530

    fix13

--- actions/post-comments/index.js
+++ actions/post-comments/index.js
@@ -10,15 +10,15 @@ const octokit = github.getOctokit(githubtoken);
             console.log('hey wr r calling v1');
             core.notice('launching actions')
             core.notice(core.getInput('GITHUB_TOKEN', {required: true}));
-            let githubtoken = core.getInput('GITHUB_TOKEN', {required: true});
-            const octokit = github.getOctokit(githubtoken);
-            let response = await octokit.rest.issues.get({
+            //let githubtoken = core.getInput('GITHUB_TOKEN', {required: true});
+            //const octokit = github.getOctokit(githubtoken);
+            /*let response = await octokit.rest.issues.get({
                 owner: 'Shankar-CodeJunkie',
                 repo: 'testgithubaction',
                 issue_number: 1
-            })
+            })*/
             //console.log(response.data.title)
-            sendComments(
+            /*sendComments(
                 'Shankar-CodeJunkie',
                 'testgithubaction',
                 1,
@@ -28,7 +28,7 @@ const octokit = github.getOctokit(githubtoken);
             let pullRequests = await getPullRequests(
                 'Shankar-CodeJunkie',
                 'testgithubaction',
-            )
+            )*/
 
             let releaseDetails = await getReleases(
                 'Shankar-CodeJunkie',
@@ -70,6 +70,7 @@ async function getPullRequests(orgName, repoName) {
 }
 
 async function getReleases(owner, repo) {
+    core.notice('coming to get release info');
     let releases = [];
     let releaseInfo = await octokit.request(
         'GET /repos/{owner}/{repo}/releases',
