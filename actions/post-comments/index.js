90cfab93065546f2e2d8c5a8fc51456284b2cf84
SHANKAR VENKATACHALAM <svenkat7@in.ibm.com>
2022-10-03 15:26:08 +0530

    fix34

--- actions/post-comments/index.js
+++ actions/post-comments/index.js
@@ -148,5 +148,7 @@ async function getCommitsBetweenTwoTags(startCommit, endCommit, owner, repo) {
 async function getPullRequestForCommit(owner, repo, commit) {
     console.log('coming to get pr for commit', commit);
     const result = await request(`GET /repos/${owner}/${repo}/commits/${commit}/pulls`).catch(err => console.log(err))
-    return result.data.map(x => x.number);
+    let pp = result.data.map(x => x.number);
+    console.log('pull r number for commit 0', commit,'==' , pp)
+    return pp;
 }
